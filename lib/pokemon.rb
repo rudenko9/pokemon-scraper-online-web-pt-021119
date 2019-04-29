1b8497080896c4c69fb156d10923abc1d434d270
Nataliya R <n.rudenko@gmail.com>
2019-04-29 20:53:12 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,6 +15,6 @@ attr_accessor :id, :name, :type, :db
     
     def self.find(id, db)
      found_pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?;" , id).flatten
-     Pokemon.new(id:, name:, type:, db:)
+     Pokemon.new(id: found_pokemon[0], name:, type:, db: db)
     end 
 end
